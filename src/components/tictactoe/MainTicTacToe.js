2245d3905aaa69ab9e87a5922f641bd73f08742e
Aleksey Petrovskiy <petrovskiy_a@itstep.org>
2020-09-30 23:29:00 +0700

    Доработана Weather APP

---
+++ src/components/tictactoe/MainTicTacToe.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class MainTicTacToe extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Крестики-нолики</h1>
+            </div>
+        )
+    }
+}
+export default MainTicTacToe;
